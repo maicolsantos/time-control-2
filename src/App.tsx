821d4dcf5c26ea6ffad107a4204f33d17ae748dc
Maicol Santos <maicolcostaa8@gmail.com>
2023-07-01 20:00:17 +0100

    Adjustment responsive

--- src/App.tsx
+++ src/App.tsx
@@ -11,128 +11,140 @@ function App() {
   }
 
   return (
-    <div className="w-full flex justify-center flex-col items-center space-y-4 py-4">
-      <h3 className="font-bold text-sm">Time Control 2</h3>
-      <Calendar
-        mode="multiple"
-        selected={selectedDate}
-        onSelect={setSelectedDate}
-        onDayClick={handleDayClick}
-        className="rounded-md border"
-      />
-
-      <div className="w-full max-w-[278px]">
-        <Card className="p-4">
-          <h3 className="mb-2 font-bold text-sm">Subtotal</h3>
-          <div className="w-full pt-2">
-            <div className="flex justify-between w-full items-start text-xs">
-              <strong className="leading-none">
-                Dias{' '}
-                <small className="text-[9px] font-bold text-gray-400">
-                  (21)
-                </small>
-              </strong>
-              <span className="font-bold leading-none">4200€</span>
-            </div>
-            <div className="flex justify-between w-full items-start text-sm text-gray-400">
-              <small className="leading-1">Previsão</small>
-              <small className="font-bold leading-1">4200€</small>
-            </div>
-          </div>
-
-          <h3 className=" mt-4 mb-2 font-bold text-sm">Descontos</h3>
-          <div className="w-full py-2 border-b border-stone-200">
-            <div className="flex justify-between w-full items-start text-xs">
-              <strong className="leading-none">
-                IVA{' '}
-                <small className="text-[9px] font-bold text-gray-400">
-                  (25%)
-                </small>
-              </strong>
-              <span className="font-bold leading-none">1000€</span>
-            </div>
-            <div className="flex justify-between w-full items-start text-xs text-gray-400">
-              <small className="leading-1">Previsão</small>
-              <small className="font-bold leading-1">4200€</small>
-            </div>
-          </div>
-          <div className="w-full pt-2">
-            <div className="flex justify-between w-full items-start text-xs">
-              <strong className="leading-none">
-                IRS{' '}
-                <small className="text-[9px] font-bold text-gray-400">
-                  (25%)
-                </small>
-              </strong>
-              <span className="font-bold leading-none">1000€</span>
-            </div>
-            <div className="flex justify-between w-full items-start text-xs text-gray-400">
-              <small className="leading-1">Previsão</small>
-              <small className="font-bold leading-1">4200€</small>
-            </div>
-          </div>
-
-          <h3 className="mt-4 mb-2 font-bold text-sm">Reembolso</h3>
-          <div className="w-full pt-2">
-            <div className="flex justify-between w-full items-start text-xs">
-              <strong className="leading-none">
-                IVA{' '}
-                <small className="text-[9px] font-bold text-gray-400">
-                  (25%)
-                </small>
-              </strong>
-              <span className="font-bold leading-none">1000€</span>
-            </div>
-            <div className="flex justify-between w-full items-start text-xs text-gray-400">
-              <small className="leading-1">Previsão</small>
-              <small className="font-bold leading-1">4200€</small>
-            </div>
-          </div>
-        </Card>
+    <>
+      <div className="max-w-3xl m-auto pt-4 px-4">
+        <h3 className="font-bold text-sm">Time Control 2</h3>
       </div>
+      <div
+        className="
+        w-full flex justify-center flex-col md:flex-row items-center
+        md:items-start space-y-4 md:space-x-4 md:space-y-0 p-4 max-w-3xl m-auto
+      "
+      >
+        <div className="w-full">
+          <Calendar
+            mode="multiple"
+            selected={selectedDate}
+            onSelect={setSelectedDate}
+            onDayClick={handleDayClick}
+            className="w-full rounded-md border"
+          />
+        </div>
+        <div className="w-full">
+          <div className="w-full mb-4">
+            <Card className="p-4">
+              <h3 className="mb-2 font-bold text-sm">Subtotal</h3>
+              <div className="w-full pt-2">
+                <div className="flex justify-between w-full items-start text-xs">
+                  <strong className="leading-none">
+                    Dias{' '}
+                    <small className="text-[9px] font-bold text-gray-400">
+                      (21)
+                    </small>
+                  </strong>
+                  <span className="font-bold leading-none">4200€</span>
+                </div>
+                <div className="flex justify-between w-full items-start text-sm text-gray-400">
+                  <small className="leading-1">Previsão</small>
+                  <small className="font-bold leading-1">4200€</small>
+                </div>
+              </div>
 
-      <div className="w-full max-w-[278px]">
-        <Card className="p-4">
-          <h3 className="mb-2 font-bold text-sm">Total</h3>
-          <div className="w-full pt-2">
-            <div className="flex justify-between w-full items-start text-xs">
-              <strong className="leading-none">
-                Saldo{' '}
-                <small className="text-[9px] font-bold text-gray-400">
-                  (+Descontos)
-                </small>
-              </strong>
-              <span className="font-bold leading-none">3000€</span>
-            </div>
-            <div className="flex justify-between w-full items-start text-xs text-gray-400">
-              <small className="leading-1">Previsão</small>
-              <small className="font-bold leading-1">3000€</small>
-            </div>
-          </div>
-        </Card>
-      </div>
+              <h3 className=" mt-4 mb-2 font-bold text-sm">Descontos</h3>
+              <div className="w-full py-2 border-b border-stone-200">
+                <div className="flex justify-between w-full items-start text-xs">
+                  <strong className="leading-none">
+                    IVA{' '}
+                    <small className="text-[9px] font-bold text-gray-400">
+                      (25%)
+                    </small>
+                  </strong>
+                  <span className="font-bold leading-none">1000€</span>
+                </div>
+                <div className="flex justify-between w-full items-start text-xs text-gray-400">
+                  <small className="leading-1">Previsão</small>
+                  <small className="font-bold leading-1">4200€</small>
+                </div>
+              </div>
+              <div className="w-full pt-2">
+                <div className="flex justify-between w-full items-start text-xs">
+                  <strong className="leading-none">
+                    IRS{' '}
+                    <small className="text-[9px] font-bold text-gray-400">
+                      (25%)
+                    </small>
+                  </strong>
+                  <span className="font-bold leading-none">1000€</span>
+                </div>
+                <div className="flex justify-between w-full items-start text-xs text-gray-400">
+                  <small className="leading-1">Previsão</small>
+                  <small className="font-bold leading-1">4200€</small>
+                </div>
+              </div>
 
-      <div className="w-full max-w-[278px]">
-        <Card className="p-4">
-          <h3 className="mb-2 font-bold text-sm">IVA Trimestre</h3>
-          <div className="w-full pt-2">
-            <div className="flex justify-between w-full items-start text-xs">
-              <strong className="leading-none">
-                IVA{' '}
-                <small className="text-[9px] font-bold text-gray-400">
-                  (25%)
-                </small>
-              </strong>
-              <span className="font-bold leading-none">3000€</span>
-            </div>
-            <div className="flex justify-between w-full items-start text-xs text-gray-400">
-              <small className="leading-1">Previsão</small>
-              <small className="font-bold leading-1">3000€</small>
-            </div>
+              <h3 className="mt-4 mb-2 font-bold text-sm">Reembolso</h3>
+              <div className="w-full pt-2">
+                <div className="flex justify-between w-full items-start text-xs">
+                  <strong className="leading-none">
+                    IVA{' '}
+                    <small className="text-[9px] font-bold text-gray-400">
+                      (25%)
+                    </small>
+                  </strong>
+                  <span className="font-bold leading-none">1000€</span>
+                </div>
+                <div className="flex justify-between w-full items-start text-xs text-gray-400">
+                  <small className="leading-1">Previsão</small>
+                  <small className="font-bold leading-1">4200€</small>
+                </div>
+              </div>
+            </Card>
           </div>
-        </Card>
+
+          <div className="w-full mb-4">
+            <Card className="p-4">
+              <h3 className="mb-2 font-bold text-sm">Total</h3>
+              <div className="w-full pt-2">
+                <div className="flex justify-between w-full items-start text-xs">
+                  <strong className="leading-none">
+                    Saldo{' '}
+                    <small className="text-[9px] font-bold text-gray-400">
+                      (+Descontos)
+                    </small>
+                  </strong>
+                  <span className="font-bold leading-none">3000€</span>
+                </div>
+                <div className="flex justify-between w-full items-start text-xs text-gray-400">
+                  <small className="leading-1">Previsão</small>
+                  <small className="font-bold leading-1">3000€</small>
+                </div>
+              </div>
+            </Card>
+          </div>
+
+          <div className="w-full">
+            <Card className="p-4">
+              <h3 className="mb-2 font-bold text-sm">IVA Trimestre</h3>
+              <div className="w-full pt-2">
+                <div className="flex justify-between w-full items-start text-xs">
+                  <strong className="leading-none">
+                    IVA{' '}
+                    <small className="text-[9px] font-bold text-gray-400">
+                      (25%)
+                    </small>
+                  </strong>
+                  <span className="font-bold leading-none">3000€</span>
+                </div>
+                <div className="flex justify-between w-full items-start text-xs text-gray-400">
+                  <small className="leading-1">Previsão</small>
+                  <small className="font-bold leading-1">3000€</small>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -20,7 +20,7 @@ function Calendar({
       className={cn('p-3', className)}
       classNames={{
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
-        month: 'space-y-4',
+        month: 'space-y-4 w-full',
         caption: 'flex justify-center pt-1 relative items-center',
         caption_label: 'text-sm font-medium',
         nav: 'space-x-1 flex items-center',
@@ -31,10 +31,10 @@ function Calendar({
         nav_button_previous: 'absolute left-1',
         nav_button_next: 'absolute right-1',
         table: 'w-full border-collapse space-y-1',
-        head_row: 'flex',
+        head_row: 'flex justify-between',
         head_cell:
           'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
-        row: 'flex w-full mt-2',
+        row: 'flex w-full mt-2 justify-between',
         cell: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
         day: cn(
           buttonVariants({ variant: 'ghost' }),
